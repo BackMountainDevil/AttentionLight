9f9f82a3bf1a8ff0342d84358e4aa7089baa017b
kearney <191615342@qq.com>
2024-12-04 11:03:54 +0800

    add extract att and plot

--- summary.py
+++ summary.py
@@ -4,6 +4,8 @@ import numpy as np
 import json
 import shutil
 import copy
+from collections import deque
+import matplotlib.pyplot as plt
 
 
 def get_metrics(duration_list, traffic_name, total_summary_metrics, num_of_out):
@@ -32,7 +34,7 @@ def summary_detail_RL(memo_rl, total_summary_rl):
     for traffic_file in os.listdir(records_dir):
         if ".json" not in traffic_file:
             continue
-        print(traffic_file)
+        # print(traffic_file)
 
         traffic_env_conf = open(os.path.join(records_dir, traffic_file, "traffic_env.conf"), 'r')
         dic_traffic_env_conf = json.load(traffic_env_conf)
@@ -177,6 +179,174 @@ def summary_detail_conventional(memo_cv):
                             sep='\t', index=False)
 
 
+def get_att_eng(memo_rl):
+    """
+    ATT from eng
+    """
+    records_dir = os.path.join("records", memo_rl)
+    traffic_files = os.listdir(records_dir)
+    traffic_files.sort()
+    att_list = []
+    for traffic_file in traffic_files:
+        if ".json" not in traffic_file:
+            continue        
+        test_round_dir = os.path.join(records_dir, traffic_file, "test_round")
+        try:
+            round_files = os.listdir(test_round_dir)
+        except:
+            print("no test round in {}".format(traffic_file))
+            continue
+        round_files = [f for f in round_files if "round" in f]
+        round_files.sort(key=lambda x: int(x[6:]))
+        att_all=[]
+        for round_rl in round_files:
+            round_dir = os.path.join(test_round_dir, round_rl)
+            df_vehicle_inter = pd.read_csv(os.path.join(round_dir, "vehicle_inter_0.csv"),
+                                            sep=',', header=0, dtype={0: str, 1: float, 2: float, 3: float},
+                                            names=["vehicle_id", "enter_time", "leave_time", "eng_att"])
+            att = df_vehicle_inter['eng_att'][0]
+            att_all.append(att)
+        last_10_att = att_all[-10:]
+        mean_att = np.mean(last_10_att)
+        att_list.append(mean_att)
+        print(f'mean_att: {mean_att:.2f} {traffic_file}')
+
+
+def extract_test_att(filepath):
+    """
+    解析单个实验中一张地图的结果，提取 att 值，保存到 csv 文件
+    filepath: 实验结果路径. 如：records/att_hz_8_6/anon_4_4_hangzhou_real_5816.json_11_27_09_33_48
+    """
+    max_episode = 80
+    att_de = deque(maxlen=max_episode)
+    if filepath.endswith("test_round"):
+        testpath = filepath
+    else:
+        testpath = os.path.join(filepath, "test_round")
+    for i in range(max_episode):
+        att_filepath = os.path.join(testpath, "round_{}".format(i), "eng-att.txt")
+        with open(att_filepath, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                att =  round(float(line),2)
+                att_de.append(att)
+    print(f"extract att from {testpath}, len={len(att_de)}")
+    # save att_de to csv
+    csv_path = os.path.join(filepath, "att.csv")
+    with open(csv_path, "w") as f:
+        for att in att_de:
+            f.write("{}, ".format(att))
+        f.write("\n")
+    print("save att to {}".format(csv_path))    
+
+def extract_exp_test_att(filepath_prefix, seeds=[6, 66, 666]):
+    """
+    解析单个实验中多个地图的结果，提取 att 值，保存到 csv 文件
+    filepath: 实验结果路径. 如：records/att_hz_8_
+    """
+    # 获取指定目录下的所有文件和子目录
+    for seed in seeds:
+        filepath = filepath_prefix + str(seed)
+        for item in os.listdir(filepath):
+            extract_test_att(os.path.join(filepath, item))
+
+def merge_exp_att_csv_plot(filepath_prefix, seeds=[6, 66, 666], max_episode = 80):
+    """
+    合并多个实验结果的 att.csv 文件，针对统一参数不同seed的实验结果
+    filepath_prefix: 实验结果路径. 如：records/att_hz_8_
+    """
+    map_path = {}
+    for seed in seeds:
+        filepath = filepath_prefix + str(seed)
+        for item in os.listdir(filepath):
+            tmp_path = os.path.join(filepath, item)
+            if not os.path.isdir(tmp_path):
+                continue
+            map_name = item.split(".")[0]
+            if map_name not in map_path.keys():
+                map_path[map_name] = []
+            map_path[map_name].append(os.path.join(filepath, item, "att.csv"))
+    print(f'find {len(map_path)} maps in {filepath_prefix}')
+    # print(map_path)
+
+    for map_name, csv_list in map_path.items():
+        # print(map_name, csv_list)
+        print(f"merge {map_name}, len={len(csv_list)}")
+        csv_data = {}   # 存储一个map,不同seed的结果
+        att_list = None
+        for csv_path in csv_list:
+            # print(f"read {csv_path}")
+            with open(csv_path, "r") as f:
+                lines = f.readlines()
+                for line in lines:
+                    att_list = list(map(float,  line.split(", ")[:-1]))
+            if len(att_list) != 0:
+                # print(f"read {csv_path}, len={len(att_list)} att_list={att_list}")
+                seed = int(csv_path.split("/")[1].split("_")[-1])
+                csv_data[seed] = att_list
+        # print(csv_data)
+                
+        # save csv_data to csv
+        plt_data = {
+            "min_att": deque(maxlen=max_episode),
+            "max_att": deque(maxlen=max_episode),
+            "mean_att": deque(maxlen=max_episode),
+        }
+        # for csv_path in csv_list:
+        #     # 保存路径不一样，但内容是一样的
+        csv_path = csv_list[0]
+        targetpath = csv_path.split("/")[:2]
+        targetpath = os.path.join(*targetpath, "{}_merged_att.csv".format(map_name))
+        att_de = deque(maxlen=len(seeds))
+        with open(targetpath, "w") as f:
+            # 写入数据表的表头
+            f.write("step, ")
+            for seed in seeds:
+                f.write("{}, ".format(seed))
+            f.write("min_att, max_att, mean_att\n") # 额外添加三个指标
+            # 写入数据内容
+            for i in range(max_episode):
+                f.write("{}, ".format(i+1))
+                for seed in seeds:
+                    f.write("{}, ".format(csv_data[seed][i]))
+                    att_de.append(csv_data[seed][i])
+                min_att = min(att_de)
+                max_att = max(att_de)
+                mean_att = round(sum(att_de)/len(seeds), 2)
+                plt_data["min_att"].append(min_att)
+                plt_data["max_att"].append(max_att)
+                plt_data["mean_att"].append(mean_att)
+                f.write("{}, {}, {}\n".format(min_att, max_att, mean_att))
+        print(f"save to {targetpath}")
+
+        
+        # # 绘制曲线图
+        plt.figure(figsize=(10, 6))
+        plt.plot(list(range(1, 81)), plt_data["mean_att"], color='blue')  # 绘制平均值曲线
+        plt.fill_between(list(range(1, 81)), plt_data["min_att"], plt_data["max_att"], color='blue', alpha=0.2) # 绘制阴影
+        plt.xlabel("step")
+        plt.ylabel("att")
+        plt.legend()    # 添加图例
+        targetpath = csv_path.split("/")[:2]
+        targetpath = os.path.join(*targetpath, "{}.png".format(map_name))
+        plt.savefig(targetpath)
+
+def merge_plot(filepath_prefix, seeds=[6, 66, 666]):
+    """
+    针对同一参数不同seed的实验结果，汇总结果+绘图。结果文件保存在第一个seed的目录下
+    """
+    for seed in seeds:
+        filepath = filepath_prefix + str(seed)
+        for item in os.listdir(filepath):
+            tmp_path = os.path.join(filepath, item)
+            if not os.path.isdir(tmp_path):
+                continue
+            extract_test_att(tmp_path)
+    
+    extract_exp_test_att(filepath_prefix,seeds)
+
+    merge_exp_att_csv_plot(filepath_prefix, seeds)
+
 if __name__ == "__main__":
     """Only use these data"""
     total_summary = {
@@ -185,6 +355,10 @@ if __name__ == "__main__":
         "final_duration_std": [],
         "final_through": [],
     }
-    memo = "benchmark_1018_15"
+    memo = "att_ny_8_666"
     # summary_detail_RL(memo, copy.deepcopy(total_summary))
-    summary_detail_conventional(memo)
+    # summary_detail_conventional(memo)
+    get_att_eng(memo)
+
+    filepath_prefix = "records/att_ny_8_"
+    merge_plot(filepath_prefix)
